9fea83cc53fac4945719873347d218340ecbe37f
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-10-06 23:29:15 +0530

    Add files via upload

---
+++ javascript3.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>javascript3</title>
+</head>
+<body>
+   <input type="text"id="name">
+   <button onclick="myfunction()">Click here</button> 
+   <p id="input1">Paragraph 1</p>
+   <script>
+    function myfunction(){
+        var x=document.getElementById("name").value;
+        document.getElementById("input1").innerHT
+        ML=x;
+    }
+   </script>
+</body>
+</html>
