51eac08a05026bf4718da7c05a46f46bd1cabbd9
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-10-08 22:58:16 +0530

    Add files via upload

---
+++ jsamulya.js
@@ -0,0 +1,17 @@
+<script>
+    var txt1 ="Amulya";
+    var txt2 ="Awasthi";
+    var txt3 ="txt1 + " "+ txt2;
+    document.write(txt3);
+    document.write("(<hr>");
+
+    var x = 5 + 5;
+    document.write(x+"<br>");
+
+    var y ="5" + 5;
+    var p = y + 6;
+    document.write(p+"<br>");
+
+    var z ="Hello" + 5;
+    dpcument.write(z+"<br>"); 
+</script>
